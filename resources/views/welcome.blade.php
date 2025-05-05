7338774bddcc87db4818c1d3a171a8758c6f2d19
Rushel <ruselaq@gmail.com>
2025-05-05 21:05:19 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yobe</title>
+</head>
+
+<body>
+    <h1>Welcome to Yobe</h1>
+    <p>Yobe is a simple and powerful PHP framework for building web applications.</p>
+    <p>To get started, check out the <a href="https://yobe.dev/docs">documentation</a>.</p>
+    <p>Happy coding!</p>
+</body>
+
+</html>
