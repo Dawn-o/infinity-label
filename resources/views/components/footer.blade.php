0b2b83dcb2373104ef4564d440779c537c770366
Rushel <ruselaq@gmail.com>
2025-05-09 15:07:17 +0800

    Adjust Icons Size

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,11 +5,12 @@
                 <div class="flex items-center space-x-3 mb-6">
                     <div
                         class="h-10 w-10 bg-gradient-to-br from-secondary to-primary rounded-lg flex items-center justify-center rotate-12">
-                        <span class="text-white font-bold text-xl -rotate-12"><svg class="w-14 h-14 text-white"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <span class="text-white font-bold text-xl -rotate-12">
+                            <svg class="w-8 h-8 text-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path fill="currentColor"
                                     d="M20.288 9.463a4.856 4.856 0 0 0-4.336-2.3 4.586 4.586 0 0 0-3.343 1.767c.071.116.148.226.212.347l.879 1.652.134-.254a2.71 2.71 0 0 1 2.206-1.519 2.845 2.845 0 1 1 0 5.686 2.708 2.708 0 0 1-2.205-1.518L13.131 12l-1.193-2.26a4.709 4.709 0 0 0-3.89-2.581 4.845 4.845 0 1 0 0 9.682 4.586 4.586 0 0 0 3.343-1.767c-.071-.116-.148-.226-.212-.347l-.879-1.656-.134.254a2.71 2.71 0 0 1-2.206 1.519 2.855 2.855 0 0 1-2.559-1.369 2.825 2.825 0 0 1 0-2.946 2.862 2.862 0 0 1 2.442-1.374h.121a2.708 2.708 0 0 1 2.205 1.518l.7 1.327 1.193 2.26a4.709 4.709 0 0 0 3.89 2.581h.209a4.846 4.846 0 0 0 4.127-7.378z" />
-                            </svg></span>
+                            </svg>
+                        </span>
                     </div>
                     <span
                         class="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-secondary to-accent">Infinity
